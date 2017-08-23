f17df155117a1f3c6f1457462ddd5c1860aafac9
ATONU GHOSH <atonughosh@outlook.com>
2017-08-23 19:13:13 +0530

    Update roll.py

--- Dice Rolling/roll.py
+++ Dice Rolling/roll.py
@@ -9,11 +9,11 @@ def roll():     #define the function to generate random number between 1 & 6
     print("Dice rolled : " + str(random.randint(1,6)) + " showed up.")
 
 roll()
-again = str(input("Roll again? (Y/N) \n"))     #check whether to roll again, take input and store in variable 'again'
+again = input("Roll again? (Y/N) \n")     #check whether to roll again, take input and store in variable 'again'
 
 #roll dice again if the uer enters Yes or yes or YES or Y or y else exit
 while (again == "Y" or again == "y" or again == "yes" or again == "Yes" or again == "YES"):
     roll()
-    again = str(input("Roll again? (Y/N) \n"))         #roll dice again if the uer enters Yes or yes or YES or Y or y
+    again = input("Roll again? (Y/N) \n")         #roll dice again if the uer enters Yes or yes or YES or Y or y
                                                     # else exit.
 
