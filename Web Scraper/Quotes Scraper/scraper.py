50d2d347c3016718fa33c85fc8809042c28ca3e3
ATONU GHOSH <atonughosh@outlook.com>
2017-08-24 00:31:48 +0530

    Update scraper.py

--- Web Scraper/Quotes Scraper/scraper.py
+++ Web Scraper/Quotes Scraper/scraper.py
@@ -1,9 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
+'''
+This scraper collects quotes from the web site http://quotes.toscrape.com 
+and displays one at a time. On first run one quote will be displayed and then
+the user is prompted to choose whether to display more quotes.
+'''
 
-r = requests.get('http://quotes.toscrape.com')
-soup = BeautifulSoup(r.content, 'lxml')
+import requests                                 #importing requests module
+from bs4 import BeautifulSoup                   #importing BeautifulSoup
 
+r = requests.get('http://quotes.toscrape.com')          #get the website contents into a request object
+soup = BeautifulSoup(r.content, 'lxml')                 #Parse the r object using the lxml parser
+
+#function to etract all quotes and return one quote at a time. The function expects and integer ar argument which 
+#is used as the index of the quotes list created with all extracted quotes
 def get_quote(i):
     #for i in range(len(soup.find_all("span",{"class":"text"}))):
     quote = (list(zip(soup.find_all("span", {"class":"text"})[i], soup.find_all("small", {"class":"author"})[i] )))
